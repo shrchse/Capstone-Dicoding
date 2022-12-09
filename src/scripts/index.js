f94a76fa91015e30eb13b05aece27d2e632a7d77
Fariz Fadillah <farizfadillah612@gmail.com>
2022-12-09 23:49:16 +0800

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,35 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import '../scripts/utils/drawer-init.js';
+import '../views/pages/my-post';
+import App from '../views/app';
+import swRegister from './utils/sw-register';
+
+const hamburgerButtonElement = document.querySelector('.hamburger');
+const drawerElement = document.querySelector('.canvas');
+const mainElement = document.querySelector('.main-content');
+
+hamburgerButtonElement.addEventListener('click', (event) => {
+  drawerElement.classList.toggle('open');
+  event.stopPropagation();
+});
+
+mainElement.addEventListener('click', (event) => {
+  drawerElement.classList.remove('open');
+  event.stopPropagation();
+});
+
+const app = new App({
+  button: document.querySelector('#hamburger'),
+  drawer: document.querySelector('#canvas'),
+  content: document.querySelector('#main'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+});
